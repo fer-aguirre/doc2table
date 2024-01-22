0a2aa2dbda47ba873fabdb8fe1409b934b50b5c0
Fer Aguirre <77854667+fer-aguirre@users.noreply.github.com>
2024-01-22 13:56:28 -0600

    Improving naming for CLI arguments

--- src/main.py
+++ src/main.py
@@ -1,92 +1,104 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 import click
 from typing import List
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.llms import OpenAI
+from langchain_openai import ChatOpenAI
 import pandas as pd
 
-
 @click.command()
-@click.argument('directory_path', type=click.Path(exists=True))
-@click.argument('file_path', type=click.Path(exists=True))
-@click.argument('token', type=click.STRING)
-def extract_information(directory_path, file_path, token):
-    pdfs, filenames = load_pdfs(directory_path)
-    questions = load_questions(file_path)
-    token = save_token(token)
+@click.option('--input-directory', type=click.Path(exists=True), required=True, help='The input directory containing PDF files.')
+@click.option('--questions-file', type=click.Path(exists=True), required=True, help='The questions file. Each question should be in a new line.')
+@click.option('--openai-api-key', type=click.STRING, envvar='OPENAI_API_KEY', required=True, help='The OpenAI API key.')
+
+def extract_pdf_information(input_directory, questions_file, openai_api_key):
+    """
+    Extract information from PDF files using provided questions and API key.
+    """
+    pdf_documents, filenames = load_pdf_files(input_directory)
+    questions_list = load_questions_from_file(questions_file)
 
     MAX_TOKENS = 512  # Maximum number of tokens allowed by the model
-    # Calculate chunk size and overlap based on maximum tokens
     chunk_size = MAX_TOKENS - 100  # Subtracting a buffer of tokens for safety
-    chunk_overlap = chunk_size // 10  # Using 10% overlap
-    docs = []
-    for pdf in pdfs:
+    chunk_overlap = chunk_size // 20  # Using 10% overlap
+    document_chunks = []
+
+    for pdf in pdf_documents:
+        """
+        Split the PDF documents into smaller chunks to handle the maximum token limit.
+        """
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, 
                                                         chunk_overlap=chunk_overlap)
-        doc_splits = text_splitter.split_documents(pdf)
-        docs.append(doc_splits)
+        document_chunks.extend(text_splitter.split_documents(pdf))
 
     results = []
-    for doc_n, doc in enumerate(docs):
+    for doc_index, document in enumerate(document_chunks):
+        """
+        Create embeddings for the documents and build a vector store for efficient searching.
+        """
         embeddings = HuggingFaceEmbeddings(model_name='paraphrase-multilingual-MiniLM-L12-v2')
-        # docsearch = Chroma.from_documents(doc, embeddings)
-        for i in range(len(doc)):
-            doc[i].metadata['source'] = f"snippet-{i}"
+        document_search = Chroma.from_documents([document], embeddings)
 
-        docsearch = Chroma.from_documents(doc, embeddings)
-
-        openai_api_key = token
+        document.metadata['source'] = f"chunk-{doc_index}"
 
         chain = RetrievalQAWithSourcesChain.from_chain_type(
-                OpenAI(model_name="gpt-3.5-turbo",
-                        temperature=0,
-                        openai_api_key=openai_api_key),
+                ChatOpenAI(model_name="gpt-3.5-turbo",
+                           temperature=0,
+                           openai_api_key=openai_api_key),
                 chain_type="stuff",
-                retriever=docsearch.as_retriever(search_type="mmr") # Max Marginal Relevance Search
+                retriever=document_search.as_retriever(search_kwargs={"k": 1})
         )
 
-        for q in questions:
-            result = {'filename': filenames[doc_n], 'question': q}
-            answer = chain({'question': f'{q}'}, return_only_outputs=True)
+        for question in questions_list:
+            """
+            Process the questions and generate answers using the chain.
+            """
+            result = {'filename': filenames[doc_index], 'question': question}
+            answer = chain.invoke({'question': f'{question}'}, return_only_outputs=True)
             result['answer'] = answer['answer']
             result['source'] = answer['sources']
             results.append(result)
-    save_results(results)
+    save_results_to_csv(results)
 
-def load_pdfs(directory_path: str) -> List[str]:
-    pdfs = []
+def load_pdf_files(input_directory: str) -> List[str]:
+    """
+    Load PDF files from the input directory and return the list of PDF documents and filenames.
+    """
+    pdf_files = []
     filenames = []
-    for file in os.listdir(directory_path):
+    for file in os.listdir(input_directory):
         if file.endswith(".pdf"):
-            path = os.path.join(directory_path, file)
+            path = os.path.join(input_directory, file)
             loader = PyPDFLoader(path)
-            pdf = loader.load()
-            pdfs.append(pdf)
+            pdf_files.append(loader.load())
             filenames.append(file)
-    return pdfs, filenames
+    return pdf_files, filenames
 
-def load_questions(file_path):
+def load_questions_from_file(questions_file):
+    """
+    Load questions from the questions file and return the list of questions.
+    """
     questions = []
-    with open(file_path, 'r') as file:
+    with open(questions_file, 'r') as file:
         for line in file:
             # Remove leading/trailing whitespaces and append to the list
             questions.append(line.strip())
     click.echo(questions)
     return questions
 
-def save_token(token):
-    # Set the environment variable with the provided token
-    os.environ['OPENAI_API_KEY'] = token
-    click.echo('Token saved as environment variable.')
-
-def save_results(results):
+def save_results_to_csv(results):
+    """
+    Save the results to a CSV file.
+    """
     df = pd.DataFrame.from_dict(results)
     df.to_csv('results.csv', index=False)
     click.echo('Results saved!')
 
 if __name__ == '__main__':
-    extract_information()
+    extract_pdf_information()
